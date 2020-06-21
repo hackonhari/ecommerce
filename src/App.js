f41b7c3e8e5bed9b3d448dc3ea6ab212fbf428ea
hackonhari <hackonhari@gmail.com>
2020-06-21 13:20:24 +0530

    section-6 completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
-import HomePage from "./pages/homepage/home.components.jsx";
-import ShopPage from './pages/shop/shop.components.jsx';
+import HomePage from "./pages/homepage/home.components";
+import ShopPage from './pages/shop/shop.components';
+import Header from './components/header/header.components';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.components';
 
 function App() {
   return (
     <div>
+    <Header/>
      <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage}/>
      </Switch>
     </div>
   );
